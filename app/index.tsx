36bc2e554a53e5bda2f54bacf99c0f75714cb537
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2025-01-10 03:09:33 -0300

    feat: Add no tasks warning

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,16 @@
 import { View, Text, TextInput } from "react-native";
+import { useState, useRef } from "react";
 import { styles } from "../assets/styles/app";
-import { PlusCircleIcon } from "react-native-heroicons/outline";
+import { PlusCircleIcon, ClipboardIcon } from "react-native-heroicons/outline";
+
+interface Task {
+    content: string;
+    done: boolean;
+}
 
 export default function App() {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
     return (
         <View style={styles.container}>
             <View style={styles.title}>
@@ -11,16 +19,39 @@ export default function App() {
                 <Text style={styles.hand}>Hand</Text>
             </View>
 
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Adicione uma nova tarefa!"
-                    placeholderTextColor={"#808080"}
-                    style={styles.textInput}
-                />
-                <View style={styles.plusIcon}>
-                    <PlusCircleIcon color="#F2F2F2" />
+            <View style={styles.content}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        placeholder="Adicione uma nova tarefa"
+                        placeholderTextColor={"#808080"}
+                        style={styles.textInput}
+                    />
+                    <View style={styles.plusIcon}>
+                        <PlusCircleIcon color="#F2F2F2" />
+                    </View>
+                </View>
+
+                <View style={styles.infoContainer}>
+                    <View style={styles.infoTextContainer}>
+                        <Text style={styles.infoText}>Criadas</Text>
+                        <Text style={styles.infoQuantity}>{tasks.length}</Text>
+                    </View>
+                    <View style={styles.infoTextContainer}>
+                        <Text style={styles.infoText}>Concluídas</Text>
+                        <Text style={styles.infoQuantity}>
+                            {tasks.filter((task) => task.done).length}
+                        </Text>
+                    </View>
                 </View>
             </View>
+
+            {tasks.length > 0 ? null: (
+                <View style={styles.noTasks}>
+                    <ClipboardIcon color="#333" size={69} />
+                    <Text style={styles.noTasksTextBold}>Você ainda não tem tarefas cadastradas</Text>
+                    <Text style={styles.noTasksText}>Crie tarefas e organize seus itens a fazer</Text>
+                </View>
+            )}
         </View>
     );
 }
